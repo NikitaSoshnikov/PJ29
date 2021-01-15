7efd2404ab07684a9b90093099e0038021137c63
NikitaSoshnikov <72586065+NikitaSoshnikov@users.noreply.github.com>
2021-01-14 19:02:41 -0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,77 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+function preload(){
+    pimg = loadImage("polygon.png");
+}
+
+function setup(){
+    var canvas = createCanvas(1200,400);
+    engine = Engine.create();
+    world = engine.world;
+    ground = new Ground (500,350,400,20)
+    b1 = new Box(400,275,30,40)
+    b2 = new Box(430,275,30,40)
+    b3 = new Box(460,275,30,40)
+    b4 = new Box(490,275,30,40)
+    b5 = new Box(520,275,30,40)
+    b6 = new Box(550,275,30,40)
+    b7 = new Box(580,275,30,40)
+
+    b8 = new Box(430,235,30,40)
+    b9 = new Box(460,235,30,40)
+    b10 = new Box(490,235,30,40)
+    b10 = new Box(520,235,30,40)
+    b11 = new Box(550,235,30,40)
+
+    b12 = new Box(460,195,30,40)
+    b13 = new Box(490,195,30,40)
+    b14 = new Box(520,195,30,40)
+    b15 = new Box(550,195,30,40)
+    b16 = new Box(490,165,30,40)
+   
+  polygon = Bodies.circle(50,200,20)
+  World.add(world,this.polygon)
+    
+  slingShot = new SlingShot(this.polygon,{x:100,y:200});
+}
+
+function draw(){
+    background("lightblue");
+    Engine.update(engine);
+
+    ground.display()
+    b1.display()
+    b2.display()
+    b3.display()
+    b4.display()
+    b5.display()
+    b6.display()
+    b7.display()
+    b8.display()
+    b9.display()
+    b10.display()
+    b11.display()
+    b12.display()
+    b13.display()
+    b14.display()
+    b15.display()
+    b16.display()
+
+    imageMode(CENTER)
+    image(pimg,this.polygon.position.x,this.polygon.position.y,40,40);
+
+    slingShot.display()
+
+
+    
+}
+
+function mouseDragged() {
+    Matter.Body.setPosition(this.polygon,{x:mouseX,y:mouseY})
+}
+function mouseReleased() {
+    slingShot.fly()
+}
